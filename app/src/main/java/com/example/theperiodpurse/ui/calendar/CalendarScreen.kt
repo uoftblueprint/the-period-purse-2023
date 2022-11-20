27ddd53855e883c774d306d6cc21a5ee44028157
Pierre-William Lessard <lessardpw@gmail.com>
2022-11-20 16:16:40 -0500

    Styled Calendar Cycle Top Bar

--- app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarScreen.kt
@@ -1,19 +1,19 @@
 package com.example.theperiodpurse.ui.calendar
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,43 +33,87 @@ class CalendarScreen : ComponentActivity() {
 }
 
 @Composable
-fun CalendarCycleTopBar () {
-    Row(
-        modifier = Modifier
-        .fillMaxWidth()
-    ) {
-        OutlinedButton(
-            onClick = { /*TODO*/ },
+fun CalendarCycleTopBar (isCalendar: Boolean) {
+    var calendarTextColor = Color.DarkGray
+    var cycleTextColor = Color.Black
+    if (isCalendar) {
+        calendarTextColor = Color.Black
+        cycleTextColor = Color.DarkGray
+    }
+
+    Column {
+        Row(
             modifier = Modifier
-                .weight(1f)
+                .fillMaxWidth()
         ) {
-            Text(text = "Calendar",
-                fontSize = 24.sp,
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RectangleShape,
+                border = BorderStroke(0.dp, color = Color.LightGray),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
                 modifier = Modifier
-                    .weight(.5f)
-                    .wrapContentWidth(Alignment.CenterHorizontally)
-                    .padding(top = 16.dp, bottom = 16.dp)
-            )
+                    .weight(1f)
+            ) {
+                Text(text = "Calendar",
+                    fontSize = 24.sp,
+                    color = calendarTextColor,
+                    modifier = Modifier
+                        .weight(.5f)
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                        .padding(top = 16.dp, bottom = 6.dp)
+                )
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RectangleShape,
+                border = BorderStroke(0.dp, color = Color.LightGray),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Cycle",
+                    fontSize = 24.sp,
+                    color = cycleTextColor,
+                    modifier = Modifier
+                        .weight(.5f)
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                        .padding(top = 16.dp, bottom = 6.dp)
+                )
+            }
         }
-        OutlinedButton(
-            onClick = { /*TODO*/ },
+        Row(
             modifier = Modifier
-                .weight(1f)
+                .fillMaxWidth()
         ) {
-            Text(text = "Cycle",
-                fontSize = 24.sp,
-                modifier = Modifier
-                    .weight(.5f)
-                    .wrapContentWidth(Alignment.CenterHorizontally)
-                    .padding(top = 16.dp, bottom = 16.dp)
+            Box(modifier = Modifier
+                .weight(.5f)
+                .size(1.dp)
+                .clip(RectangleShape)
+                .background(color = calendarTextColor)
+            )
+            Box(modifier = Modifier
+                .weight(.5f)
+                .size(5.dp)
+                .clip(RectangleShape)
+                .background(color = Color.LightGray)
             )
         }
     }
+
+}
+
+fun CalendarBottomBar () {
+
 }
 
 @Composable
 fun CalendarScreenLayout() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
+        CalendarCycleTopBar(true)
+    }
 }
 
 @Preview
@@ -81,5 +125,5 @@ fun CalendarScreenPreview() {
 @Preview
 @Composable
 fun CalendarCycleTopBarPreview () {
-    CalendarCycleTopBar()
+    CalendarCycleTopBar(true)
 }
