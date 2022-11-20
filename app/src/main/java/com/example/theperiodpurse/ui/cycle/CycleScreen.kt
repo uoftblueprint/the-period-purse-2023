7d3d67a13b3820c5769553347d05819610794f5a
Manpreet Bhatti <manpreet@bhatti.net>
2022-11-19 21:07:08 -0500

    Convert static strings to string resources

--- app/src/main/java/com/example/theperiodpurse/ui/cycle/CycleScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/cycle/CycleScreen.kt
@@ -14,10 +14,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.theperiodpurse.R
 import com.example.theperiodpurse.ui.theme.ThePeriodPurseTheme
 
 class CycleScreen : ComponentActivity() {
@@ -42,7 +44,7 @@ fun CurrentCycleBox() {
         elevation = 2.dp, shape = RoundedCornerShape(5)
     ) {
         Text(
-            text = "Current cycle",
+            text = stringResource(R.string.current_cycle),
             fontSize = 20.sp,
             fontWeight = FontWeight(700),
             color = Color(0xFFB12126),
@@ -67,7 +69,7 @@ fun AverageLengthBox(title: String, image: String = "Change this into Image late
             Spacer(modifier = Modifier.height(20.dp))
             Row {
                 Text(
-                    text = "Please start logging to learn more.",
+                    text = stringResource(R.string.log_to_learn),
                     fontSize = 10.sp,
                     fontWeight = FontWeight(500),
                     modifier = Modifier.width(55.dp)
@@ -89,14 +91,14 @@ fun CycleHistoryBox() {
     RoundedCornerShape(10)) {
         Column(Modifier.padding(horizontal = 30.dp, vertical = 25.dp)) {
             Text(
-                text = "Cycle history",
+                text = stringResource(R.string.cycle_history),
                 fontSize = 20.sp,
                 fontWeight = FontWeight(700),
                 color = Color(0xFF868083),
             )
             Divider(color = Color(0xFF868083), modifier = Modifier
                 .padding(vertical = 15.dp).fillMaxWidth())
-            Text(text = "Please start logging to learn more.")
+            Text(text = stringResource(R.string.log_to_learn))
         }
     }
 }
@@ -116,9 +118,11 @@ fun CycleScreenLayout() {
         CurrentCycleBox()
         Spacer(modifier = Modifier.height(30.dp))
         Row {
-            AverageLengthBox(title = "Average period length", color = Color(0xFFFEDBDB))
+            AverageLengthBox(title = stringResource(R.string.avg_period_len),
+                color = Color(0xFFFEDBDB))
             Spacer(modifier = Modifier.width(16.dp))
-            AverageLengthBox(title = "Average cycle length", color = Color(0xFFBAE0D8))
+            AverageLengthBox(title = stringResource(R.string.avg_cycle_len),
+                color = Color(0xFFBAE0D8))
         }
         Spacer(modifier = Modifier.height(30.dp))
         CycleHistoryBox()
