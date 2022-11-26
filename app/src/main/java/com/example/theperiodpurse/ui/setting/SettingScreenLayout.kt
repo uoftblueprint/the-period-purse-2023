4d5b641ae0c5584bdc5aa45b7f773c0212a57500
manya-mittal <manyamittal614@gmail.com>
2022-11-25 23:03:47 -0500

    Build basic layout of main settings page

---
+++ app/src/main/java/com/example/theperiodpurse/ui/setting/SettingScreenLayout.kt
@@ -0,0 +1,159 @@
+package com.example.theperiodpurse.ui.setting
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.theperiodpurse.R
+import com.example.theperiodpurse.ui.theme.ThePeriodPurseTheme
+
+//class SettingScreen {
+//}
+
+@Composable
+fun SettingScreenLayout(modifier: Modifier = Modifier){
+   Column(modifier = modifier
+       .fillMaxSize()
+       .verticalScroll(rememberScrollState())) {
+       Text(
+           text = stringResource(R.string.tracking_preferences),
+           modifier = modifier.padding(
+               top = 50.dp,
+               start = 10.dp))
+       Row(modifier = modifier
+           .fillMaxWidth()
+           .padding(30.dp),
+           verticalAlignment = Alignment.CenterVertically,
+           horizontalArrangement = Arrangement.Center) {
+           TrackingOptionButton(
+               modifier = modifier,
+               label = stringResource(R.string.mood),
+               icon = painterResource(id = R.drawable.sentiment_neutral_black_24dp)
+           )
+           TrackingOptionButton(
+               modifier = modifier,
+               label = stringResource(R.string.exercise),
+               icon = painterResource(id = R.drawable.self_improvement_black_24dp)
+           )
+           TrackingOptionButton(
+               modifier = modifier,
+               label = stringResource(R.string.cramps),
+               icon = painterResource(id = R.drawable.sick_black_24dp)
+           )
+           TrackingOptionButton(
+               modifier = modifier,
+               label = stringResource(R.string.sleep),
+               icon = painterResource(id = R.drawable.nightlight_black_24dp)
+           )
+
+
+       }
+
+       Text(
+           text = stringResource(R.string.notifications_heading),
+           modifier = modifier.padding(
+               top = 50.dp,
+               start = 10.dp)
+       )
+       Row(modifier = modifier.padding(20.dp)) {
+           Text(text = stringResource(R.string.remind_me_to_log_symptoms))
+           Switch(
+               checked = false,
+               onCheckedChange = { /* TODO: change*/},
+               modifier = modifier
+                   .fillMaxWidth()
+                   .wrapContentWidth(Alignment.End),
+               colors = SwitchDefaults.colors(
+                   uncheckedThumbColor = Color.DarkGray
+               )
+           )
+       }
+       Divider(thickness = 1.dp)
+
+      TabOption(stringResource(id = R.string.customize_notifications))
+
+       Divider(thickness = 1.dp)
+       Text(
+           text = stringResource(R.string.account_settings_heading),
+           modifier = modifier.padding(
+               top = 50.dp,
+               start = 10.dp)
+       )
+
+       TabOption(text = stringResource(R.string.back_up_account))
+       Spacer(modifier = Modifier.padding(10.dp))
+       TabOption(text = stringResource(id = R.string.delete_account))
+   }
+
+}
+
+@Composable
+fun TrackingOptionButton(modifier: Modifier, label: String, icon: Painter) {
+    Column(
+        modifier = modifier
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,) {
+        IconButton(
+            modifier = Modifier
+                .height(70.dp)
+                .width(70.dp),
+            onClick = { /*TODO*/ }) {
+            Icon(
+                painter = icon,
+                "contentDescription")
+
+        }
+        Text(modifier = Modifier.padding(5.dp),
+            text = label)
+    }
+}
+
+@Composable
+fun TabOption(text: String){
+    TabRow(modifier = Modifier.fillMaxWidth(), selectedTabIndex = 0, backgroundColor = Color.White) {
+        Tab(modifier = Modifier.fillMaxWidth(), selected = true, onClick = { /*TODO*/ }) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = text)
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowRight,
+                    contentDescription = "arrow",
+                    modifier = Modifier.wrapContentWidth(Alignment.End)
+                )
+            }
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SettingScreenScreenPreview() {
+    ThePeriodPurseTheme {
+        SettingScreenLayout()
+    }
+}
