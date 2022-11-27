a80932310361cdf5674ee70d3df9dee1ece3524c
michael <michaelng010702@gmail.com>
2022-11-27 14:32:22 -0500

    modularized FAB

--- app/src/main/java/com/example/theperiodpurse/AppScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/AppScreen.kt
@@ -7,11 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.example.theperiodpurse.ui.theme.ThePeriodPurseTheme
 import com.example.theperiodpurse.ui.component.BottomNavigation
-import com.example.theperiodpurse.ui.component.FloatingActionButton
 
 enum class AppScreen() {
     Calendar,
@@ -40,29 +38,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    var fabIcon by remember { mutableStateOf(R.drawable.add_circle_outline_black_24dp) }
-    var backgroundColor by remember {
-        mutableStateOf(Color(0xFFBF3428))
-    }
-    navController.addOnDestinationChangedListener { _, destination, _ ->
-        if (destination.route == AppScreen.Calendar.name) {
-            fabIcon = R.drawable.add_black_24dp
-            backgroundColor = Color(0xFFBF3428)
-        } else {
-            fabIcon = R.drawable.today_black_24dp
-            backgroundColor = Color(0xFF72C6B7)
-        }
-    }
     Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(navController, fabIcon, backgroundColor)
-        },
         bottomBar = {
             BottomNavigation(navController = navController)
         },
-
-        floatingActionButtonPosition = FabPosition.Center,
-        isFloatingActionButtonDocked = true,
     ) {
         NavigationGraph(navController = navController)
     }
--- app/src/main/java/com/example/theperiodpurse/ui/component/CommonComponent.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/component/CommonComponent.kt
@@ -1,32 +1,38 @@
 
 package com.example.theperiodpurse.ui.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.theperiodpurse.AppScreen
 import com.example.theperiodpurse.R
+import com.example.theperiodpurse.ui.theme.Red100
+import com.example.theperiodpurse.ui.theme.Teal100
 
 @Composable
 fun BottomNavigation(navController: NavController) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
+    var fabIcon by remember { mutableStateOf(R.drawable.add_circle_outline_black_24dp) }
+    var fabBackgroundColor by remember { mutableStateOf(Red100) }
+    navController.addOnDestinationChangedListener { _, destination, _ ->
+        if (destination.route == AppScreen.Calendar.name) {
+            fabIcon = R.drawable.add_black_24dp
+            fabBackgroundColor = Red100
+        } else {
+            fabIcon = R.drawable.today_black_24dp
+            fabBackgroundColor = Teal100
+        }
+    }
+
     BottomNavigation_(
         onInfoNavigationClicked = {
             navController.navigate(AppScreen.Learn.name) {
@@ -45,20 +51,34 @@ fun BottomNavigation(navController: NavController) {
                 launchSingleTop = true
                 restoreState = true
             }
-        }
+        },
+        onFABClicked = {
+            navController.navigate(AppScreen.Calendar.name) {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                launchSingleTop = true
+                restoreState = true
+            }
+        },
+        fabIcon = fabIcon,
+        fabBackgroundColor = fabBackgroundColor
     )
 }
 
 @Composable
 private fun BottomNavigation_(
-        onInfoNavigationClicked: () -> Unit,
-        onSettingsNavigationClicked: () -> Unit,
-        modifier: Modifier = Modifier
-    ) {
+    onInfoNavigationClicked: () -> Unit,
+    onSettingsNavigationClicked: () -> Unit,
+    onFABClicked: () -> Unit,
+    fabIcon: Int,
+    fabBackgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     Box() {
         BottomNavigation(
             backgroundColor = Color.White,
-            modifier = modifier
+            modifier = modifier.align(Alignment.BottomCenter)
         ) {
 
             BottomNavigationItem(
@@ -72,6 +92,7 @@ private fun BottomNavigation_(
                 selected = false,
                 onClick = onInfoNavigationClicked,
             )
+
 
             BottomNavigationItem(
                 icon = {},
@@ -90,31 +111,37 @@ private fun BottomNavigation_(
                     )
                 },
                 label = { Text(AppScreen.Settings.name) },
-//                selected = currentDestination?.hierarchy?.any { it.route == AppScreen.Settings.name } == true,
                 selected = false,
                 onClick = onSettingsNavigationClicked,
             )
         }
+
+        FloatingActionButton(
+            onClick = onFABClicked,
+            id = fabIcon,
+            backgroundColor = fabBackgroundColor,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(15.dp)
+        )
+
     }
 }
 
 @Composable
-fun FloatingActionButton(navController: NavController, id: Int, backgroundColor: Color) {
+fun FloatingActionButton(
+    onClick: () -> Unit,
+    id: Int,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     val circle = MaterialTheme.shapes.large.copy(CornerSize(percent = 50))
     FloatingActionButton(
-        onClick = {
-            navController.navigate(AppScreen.Calendar.name) {
-                popUpTo(navController.graph.findStartDestination().id) {
-                    saveState = true
-                }
-                launchSingleTop = true
-                restoreState = true
-            }
-        },
+        onClick = onClick,
         shape = circle,
         contentColor = Color.White,
         backgroundColor = backgroundColor,
-        modifier = Modifier
+        modifier = modifier
             .border(color = Color.White, width = 2.dp, shape = circle)
             .size(70.dp)
     ) {
@@ -130,8 +157,13 @@ fun FloatingActionButton(navController: NavController, id: Int, backgroundColor:
 
 @Preview
 @Composable
-fun BottomNavigationPreview() {
-    BottomNavigation_({}, {})
+fun BottomNavigationPreviewNonCalendar() {
+    BottomNavigation_({}, {}, {}, R.drawable.today_black_24dp, Teal100)
 }
 
+@Preview
+@Composable
+fun BottomNavigationPreviewCalendar() {
+    BottomNavigation_({}, {}, {}, R.drawable.add_black_24dp, Red100)
+}
 
