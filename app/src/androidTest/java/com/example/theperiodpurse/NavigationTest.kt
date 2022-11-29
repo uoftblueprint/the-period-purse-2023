d6271a80f52518dcb7d2b868a2da79f0602a16fb
michael <michaelng010702@gmail.com>
2022-11-29 12:41:49 -0500

    added dependencies and tests

---
+++ app/src/androidTest/java/com/example/theperiodpurse/NavigationTest.kt
@@ -0,0 +1,70 @@
+package com.example.theperiodpurse
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupNavHost() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            ScreenApp(navController = navController)
+        }
+    }
+
+    @Test
+    fun appNavhost_clickSettings_navigatesToSettingsScreen() {
+        composeTestRule.onNodeWithText(Screen.Settings.name)
+            .performClick()
+        navController.assertCurrentRouteName(Screen.Settings.name)
+    }
+
+    private fun navigateToSettingsScreen() {
+        composeTestRule.onNodeWithText(Screen.Settings.name).performClick()
+    }
+
+    @Test
+    fun appNavhost_clickSettings_navigatesToInfoScreen() {
+        composeTestRule.onNodeWithText(Screen.Learn.name)
+            .performClick()
+        navController.assertCurrentRouteName(Screen.Learn.name)
+    }
+
+    private fun navigateToInfoScreen() {
+        composeTestRule.onNodeWithText(Screen.Learn.name).performClick()
+    }
+
+    @Test
+    fun appNavHost_clickCalendarFABOnSettingsScreen_navigatesToCalendarScreen() {
+        navigateToSettingsScreen()
+        composeTestRule.onNodeWithContentDescription("fab")
+            .performClick()
+        navController.assertCurrentRouteName(Screen.Calendar.name)
+    }
+
+    @Test
+    fun appNavHost_clickCalendarFABOnInfoScreen_navigatesToCalendarScreen() {
+        navigateToInfoScreen()
+        composeTestRule.onNodeWithContentDescription("fab")
+            .performClick()
+        navController.assertCurrentRouteName(Screen.Calendar.name)
+    }
+
+}
