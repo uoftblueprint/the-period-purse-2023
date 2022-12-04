2e043a609f27386fc468a4e8299b95cc2e0c8560
manya-mittal <manyamittal614@gmail.com>
2022-12-04 16:18:22 -0500

    Making Settings Navigation Tests

---
+++ app/src/androidTest/java/com/example/theperiodpurse/NavigationTest.kt
@@ -0,0 +1,58 @@
+package com.example.theperiodpurse
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.theperiodpurse.ui.setting.SettingScreenNavigation
+import com.example.theperiodpurse.ui.setting.SettingsScreen
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupNavHost() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+//            ScreenApp(navController = navController)
+            SettingsScreen(navController = navController)
+        }
+    }
+
+    @Test
+    fun appNavhost_clickNotifications_navigatesToNotificationsScreen() {
+        navController.assertCurrentRouteName(SettingScreenNavigation.Start.name)
+        composeTestRule.onNodeWithStringId(R.string.customize_notifications)
+            .performClick()
+        navController.assertCurrentRouteName(SettingScreenNavigation.Notification.name)
+    }
+
+    @Test
+    fun appNavhost_clickBackUp_navigatesToBackUpScreen() {
+        navController.assertCurrentRouteName(SettingScreenNavigation.Start.name)
+        composeTestRule.onNodeWithStringId(R.string.back_up_account)
+            .performClick()
+        navController.assertCurrentRouteName(SettingScreenNavigation.BackUpAccount.name)
+    }
+
+    @Test
+    fun appNavhost_clickDelete_navigatesToDeleteScreen() {
+        navController.assertCurrentRouteName(SettingScreenNavigation.Start.name)
+        composeTestRule.onNodeWithStringId(R.string.delete_account)
+            .performClick()
+        navController.assertCurrentRouteName(SettingScreenNavigation.DeleteAccount.name)
+    }
+
+}
---
+++ app/src/androidTest/java/com/example/theperiodpurse/ScreenAssertions.kt
@@ -0,0 +1,18 @@
+package com.example.theperiodpurse
+
+import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.NavController
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Assert.assertEquals
+
+fun NavController.assertCurrentRouteName(expectedRouteName: String) {
+    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithStringId(
+    @StringRes id: Int
+): SemanticsNodeInteraction = onNodeWithText(activity.getString(id))
