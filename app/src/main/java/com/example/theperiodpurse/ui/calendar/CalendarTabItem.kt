258a725e27ebf1a31e629ca312be738219b13c9a
Pierre-William Lessard <lessardpw@gmail.com>
2022-12-06 20:08:42 -0500

    Fixed RrequiresApi causing issues

--- app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarTabItem.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarTabItem.kt
@@ -1,5 +1,7 @@
 package com.example.theperiodpurse.ui.calendar
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import com.example.theperiodpurse.ui.cycle.CycleScreenLayout
 
@@ -7,6 +9,7 @@ typealias ComposableFun = @Composable () -> Unit
 
 sealed class CalendarTabItem(var title: String, var screen: ComposableFun) {
     // Sealed Class to separately store the tab data from any screens main file
+    @RequiresApi(Build.VERSION_CODES.O)
     object CalendarTab : CalendarTabItem("Calendar", { CalendarScreenLayout() })
     object CycleTab : CalendarTabItem("Cycle", { CycleScreenLayout() })
 }
