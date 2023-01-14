d2f99800cb02feca570edbfc5f69c09e6a2cc7d4
Manpreet Bhatti <manpreet@bhatti.net>
2023-01-14 04:22:36 -0500

    Created preferences keys

--- app/src/main/java/com/example/theperiodpurse/data/User.kt
+++ app/src/main/java/com/example/theperiodpurse/data/User.kt
@@ -1,5 +1,15 @@
 package com.example.theperiodpurse.data
 
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
+import com.google.gson.Gson
+import java.io.IOException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+
 data class User(
     val symptomsToTrack: ArrayList<Symptom> = ArrayList(),
     val periodHistory: ArrayList<Date> = ArrayList(),
@@ -7,3 +17,15 @@ data class User(
     val averageCycleLength: Int = 0,
     val daysSinceLastPeriod: Int = 0
     )
+
+class UserPreferencesRepository(private val dataStore: DataStore<Preferences>) {
+    private val TAG: String = "UserPreferencesRepo"
+
+    private object PreferencesKeys {
+        val SYMPTOMS_TO_TRACK = stringPreferencesKey("symptoms_to_track")
+        val PERIOD_HISTORY = stringPreferencesKey("period_history")
+        val AVERAGE_PERIOD_LENGTH = intPreferencesKey("average_period_length")
+        val AVERAGE_CYCLE_LENGTH = intPreferencesKey("average_cycle_length")
+        val DAYS_SINCE_LAST_PERIOD = intPreferencesKey("days_since_last_period")
+    }
+}
