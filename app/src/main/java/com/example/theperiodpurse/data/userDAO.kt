23ae950a8bc2e53dde667cbb6d7b36590b276f1f
Manpreet Bhatti <manpreet@bhatti.net>
2023-01-22 15:50:01 -0500

    Updated userDAO

--- app/src/main/java/com/example/theperiodpurse/data/userDAO.kt
+++ app/src/main/java/com/example/theperiodpurse/data/userDAO.kt
@@ -1,8 +1,41 @@
 package com.example.theperiodpurse.data
 
-interface userDAO {
-    fun get(): User
-    fun save(user: User): User
-    fun update(user: User): User
-    fun delete(): Unit
+import javax.persistence.EntityManager
+import javax.persistence.Persistence
+
+class UserDAO {
+    private val em: EntityManager
+    private val emf =
+        Persistence.createEntityManagerFactory("persistenceUnit")
+
+    init {
+        em = emf.createEntityManager()
+    }
+
+    fun create(user: User) {
+        em.transaction.begin()
+        em.persist(user)
+        em.transaction.commit()
+    }
+
+    fun findById(id: Long): User? {
+        return em.find(User::class.java, id)
+    }
+
+    fun update(user: User) {
+        em.transaction.begin()
+        em.merge(user)
+        em.transaction.commit()
+    }
+
+    fun delete(user: User) {
+        em.transaction.begin()
+        em.remove(user)
+        em.transaction.commit()
+    }
+
+    fun destroy() {
+        em.close()
+        emf.close()
+    }
 }
