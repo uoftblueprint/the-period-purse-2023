bd749499e8f9f21261124ee237637ddc304a2c84
michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
2023-01-22 14:31:31 -0500

    added tests

---
+++ app/src/androidTest/java/com/example/theperiodpurse/CalendarScreenTest.kt
@@ -0,0 +1,39 @@
+package com.example.theperiodpurse
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CalendarScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupNavHost() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            ScreenApp(navController = navController, skipOnboarding = true)
+        }
+    }
+
+    @Test
+    fun appLogScreen_clickOnArrow_showAndHideSymptomOptions() {
+        composeTestRule.onNodeWithContentDescription("Expand to switch symptoms")
+            .performClick()
+        composeTestRule.onNodeWithTag("Symptom Options").assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Expand to switch symptoms")
+            .performClick()
+        composeTestRule.onNodeWithTag("Symptom Options").assertIsNotDisplayed()
+    }
+}
--- app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -120,7 +121,7 @@ private fun SwitchSymptomTab(
 ) {
     Row(
         horizontalArrangement = Arrangement.SpaceEvenly,
-        modifier = modifier
+        modifier = modifier.testTag("Symptom Options")
     ) {
         symptoms.zip(onSymptomOnClicks).forEach { (symptom, onClick) ->
             val interactionSource = remember { MutableInteractionSource() }
