9d974376e36de1392da17d909bba63a46402aff3
Manpreet Bhatti <manpreet@bhatti.net>
2023-02-04 22:12:49 -0500

    Update get function to be a suspend function

--- app/src/main/java/com/example/theperiodpurse/data/userDAO.kt
+++ app/src/main/java/com/example/theperiodpurse/data/userDAO.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 @Dao
 interface UserDAO {
     @Query("SELECT * FROM users WHERE id = :id")
-    fun get(id: Int): User
+    suspend fun get(id: Int): User
     @Query("SELECT * FROM users")
     fun getUsers(): List<User>
     @Insert(onConflict = OnConflictStrategy.REPLACE)
