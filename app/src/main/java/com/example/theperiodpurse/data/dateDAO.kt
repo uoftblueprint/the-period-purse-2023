cf7c9f994e6da61488660c03b048e664e501eca6
Manpreet Bhatti <manpreet@bhatti.net>
2023-02-05 16:26:05 -0500

    Update get() function

--- app/src/main/java/com/example/theperiodpurse/data/dateDAO.kt
+++ app/src/main/java/com/example/theperiodpurse/data/dateDAO.kt
@@ -1,12 +1,11 @@
 package com.example.theperiodpurse.data
 
 import androidx.room.*
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DateDAO {
     @Query("SELECT * FROM dates WHERE id = :id")
-    fun get(id: Int): Flow<Date>
+    fun get(id: Int): Date
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun save(date: Date)
     @Update
