c68d6b09b46f8c88e2f8b2ec06202c3eb542665e
Madeline <93456777+madelahn@users.noreply.github.com>
2023-02-05 16:50:12 -0500

    49 prettify calendar page (#52)
    * Added font, fixed font weights in CalendarScreen.kt
    
    * removed whitespace behind FAB
    
    * Added Status Bar color and better headercolor
    
    * Adjusted fonts and FontWeighs for CalendarScreen
    
    * Added tab ripple fix for android vers pre 13
    
    * Added nav bar color
    
    * Removed Padding on Calendar
    
    * Fixed navigating to unnavigable dates
    
    * Added Color Standardization
    
    * Fix merge import issues
    
    ---------
    
    Co-authored-by: michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
    Co-authored-by: Pierre-William Lessard <lessardpw@gmail.com>
    Co-authored-by: Pierre-William Lessard <43834016+PierreLessard@users.noreply.github.com>

--- app/src/main/java/com/example/theperiodpurse/ui/theme/Theme.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -37,6 +39,14 @@ fun ThePeriodPurseTheme(
     } else {
         LightColorPalette
     }
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = HeaderColor1
+    )
+    systemUiController.setNavigationBarColor(
+        color = BottomBarColor1
+    )
 
     MaterialTheme(
         colors = colors,
