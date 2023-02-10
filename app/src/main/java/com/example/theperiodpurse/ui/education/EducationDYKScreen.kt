5743f1f10fe970841f167bef5de4c87559f1ba70
Madeline <madeline.ahn@mail.utoronto.ca>
2023-02-09 21:30:55 -0500

    Added navigation, created basic layout for Education Home Page

--- app/src/main/java/com/example/theperiodpurse/ui/education/EducationDYKScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/education/EducationDYKScreen.kt
@@ -1,2 +1,12 @@
 package com.example.theperiodpurse.ui.education
 
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+
+@Composable
+fun EducationDYKScreen() {
+    Column() {
+        Text("Did you know?")
+    }
+}
--- app/src/main/java/com/example/theperiodpurse/ui/education/EducationInfoScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/education/EducationInfoScreen.kt
@@ -1,2 +1,27 @@
 package com.example.theperiodpurse.ui.education
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.theperiodpurse.R
+
+@Composable
+fun EducationInfoScreen(
+    elementId: String,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Image(painter = painterResource(R.drawable.app_logo), contentDescription = "")
+        Text(elementId)
+
+    }
+}
