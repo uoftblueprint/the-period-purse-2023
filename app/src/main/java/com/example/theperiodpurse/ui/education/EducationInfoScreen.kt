63b6f6ef9ae98c9e0ba02a8c39b92ba74f8a0f30
Madeline <madeline.ahn@mail.utoronto.ca>
2023-02-11 13:04:02 -0500

    Added education images; fixed layout for EducationHomeScreen.kt

--- app/src/main/java/com/example/theperiodpurse/ui/education/EducationInfoScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/education/EducationInfoScreen.kt
@@ -9,12 +9,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.theperiodpurse.R
 
 @Composable
 fun EducationInfoScreen(
     elementId: String,
 ) {
+    EducationBackground()
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -24,4 +27,10 @@ fun EducationInfoScreen(
         Text(elementId)
 
     }
+}
+
+@Preview
+@Composable
+fun EducationInfoPreview() {
+    EducationInfoScreen("Pads")
 }
