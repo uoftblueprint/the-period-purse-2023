daef6d865156e811b0b3ca33c99e7a613307abe8
Manpreet Bhatti <manpreet@bhatti.net>
2023-02-11 22:47:28 -0500

    Make the DAOs @Singletons as well

--- app/src/main/java/com/example/theperiodpurse/data/module/DatabaseModule.kt
+++ app/src/main/java/com/example/theperiodpurse/data/module/DatabaseModule.kt
@@ -15,11 +15,13 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 private object DatabaseModule {
     @Provides
+    @Singleton
     fun provideUserDao(appDatabase: ApplicationRoomDatabase): UserDAO {
         return appDatabase.userDAO()
     }
 
     @Provides
+    @Singleton
     fun provideDateDao(appDatabase: ApplicationRoomDatabase): DateDAO {
         return appDatabase.dateDAO()
     }
