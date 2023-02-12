c17122e4819a613fec6fb21e3f63ba898614689b
Madeline <madeline.ahn@mail.utoronto.ca>
2023-02-12 18:32:23 -0500

    Adjusted visuals for EducationHomeScreen.kt

--- app/src/main/java/com/example/theperiodpurse/ui/education/EducationHomeScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/education/EducationHomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.theperiodpurse.ui.education
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,11 +16,13 @@ import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.theperiodpurse.R
@@ -28,11 +31,14 @@ import com.example.theperiodpurse.R
 val products = listOf("Period Underwear", "Menstrual Cup", "Pads", "Cloth Pads", "Tampons",
     "Menstrual Disc")
 
+const val gray = 0xFF6D6E71
+const val teal = 0xFF72C6B7
+const val pink = 0xFFFFA3A4
+
 
 /**
  * Education Home Screen for the Learn/Info Tab on the App.
  */
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun EducationScreen(
     navController: NavHostController,
@@ -55,47 +61,17 @@ fun EducationScreen(
                         .fillMaxWidth()
                         .align(Alignment.CenterHorizontally)
                 ) {
-                    Card(
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .padding(12.dp),
 
-                        shape = RoundedCornerShape(12.dp),
-                        elevation = 8.dp,
-                        backgroundColor = Color.Gray,
-                        onClick = { navController.navigate(Destination.DYK.route) },
-                    ) {
-                        Row {
-                            Column(
-//                                modifier = Modifier.wrapContentSize(Alignment.Center),
-                                verticalArrangement = Arrangement.Center
-                            ) {
-                                Text(modifier = Modifier
-                                    .width(200.dp)
-                                    .padding(vertical = 18.dp, horizontal = 24.dp),
-                                    textAlign = TextAlign.Left,
-                                    color = Color.White,
-                                    text = "Did you know?")
-
-                                Text(modifier = Modifier
-                                    .width(200.dp)
-                                    .padding(horizontal = 24.dp),
-                                    textAlign = TextAlign.Left,
-                                    text = "There are 4 phases in your cycle: ...")
-                            }
-
-                            Image(modifier = Modifier
-                                .height(120.dp)
-                                .padding(12.dp),
-                                painter = painterResource(R.drawable.dykpad),
-                                contentDescription = null
-                            )
-                        }
+                    /* DYK Card Facts */
+                    Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        DYKCard(navController)
                     }
 
                     Text(
                         modifier = Modifier.padding(8.dp),
                         textAlign = TextAlign.Left,
+                        color = Color(gray),
+                        fontWeight = FontWeight.W800,
                         text = "Tap to learn more about period products."
                     )
                 }
@@ -107,17 +83,19 @@ fun EducationScreen(
                 PeriodProducts(navController, it)
             }
 
+            /* Bottom Section */
             item (
                 span = {GridItemSpan(2)}
             ) {
-                Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Column {
                     TPPCard(uriHandler)
 
-                    /*
-                    TODO: Add Social Media Icons
-                    */
+                    Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        SocialMedia(uriHandler)
+                    }
 
-                    Text(modifier = Modifier.padding(horizontal = 8.dp, vertical = 12.dp)
+                    Text(modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 12.dp)
                         .align(Alignment.CenterHorizontally),
                         text = "© 2023 The Period Purse. All rights reserved.",
                         textAlign = TextAlign.Center,
@@ -127,35 +105,11 @@ fun EducationScreen(
                     /*
                     Terms & Conditions, and Privacy Policy
                      */
-                    Row(
-                        modifier = Modifier
-                            .wrapContentHeight()
-                            .padding(horizontal = 12.dp)
-                            .align(Alignment.CenterHorizontally),
-                        horizontalArrangement = Arrangement.Center,
-                        ) {
-                        ClickableText(
-                            text = AnnotatedString("Terms and Conditions"),
-                            onClick = { navController.navigate(Destination.Terms.route) },
-                            style = TextStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = Color.Blue,
-                                textAlign = TextAlign.Center
-                            )
-                        )
-                        Text(" and ")
-                        ClickableText(
-                            text = AnnotatedString("Privacy Policy"),
-                            onClick = { navController.navigate(Destination.Privacy.route) },
-                            style = TextStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = Color.Blue,
-                                textAlign = TextAlign.Center
-                            )
-                        )
+                    Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        TermsAndPrivacyFooter(navController)
                     }
 
-                    Spacer(modifier = Modifier.size(32.dp))
+                    Spacer(modifier = Modifier.size(64.dp))
 
                 }
             }
@@ -163,6 +117,47 @@ fun EducationScreen(
     }
 }
 
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DYKCard(navController: NavHostController) {
+    Card(
+        modifier = Modifier
+            .padding(12.dp),
+
+        shape = RoundedCornerShape(12.dp),
+        elevation = 10.dp,
+        backgroundColor = Color(teal),
+        onClick = { navController.navigate(Destination.DYK.route) },
+    ) {
+        Row {
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 18.dp, horizontal = 24.dp),
+                    textAlign = TextAlign.Left,
+                    color = Color.White,
+                    fontWeight = FontWeight.W800,
+                    text = "Did you know?")
+
+                Text(modifier = Modifier
+                    .width(200.dp)
+                    .padding(horizontal = 24.dp),
+                    textAlign = TextAlign.Left,
+                    text = "There are 4 phases in your cycle: ...")
+            }
+
+            Image(modifier = Modifier
+                .height(120.dp)
+                .padding(12.dp),
+                painter = painterResource(R.drawable.dykpad),
+                contentDescription = null
+            )
+        }
+    }
+}
 
 /**
  * Grid View for different Period Products.
@@ -172,16 +167,16 @@ fun EducationScreen(
 fun PeriodProducts (navController: NavHostController, it: String) {
     Card(
         modifier = Modifier
-            .padding(8.dp)
+            .padding(12.dp)
             .height(IntrinsicSize.Min)
             .aspectRatio(1f),
-        shape = RoundedCornerShape(12.dp), elevation = 8.dp,
-        backgroundColor = Color.Gray,
+        shape = RoundedCornerShape(12.dp), elevation = 10.dp,
+        backgroundColor = Color(pink),
         onClick = { navController.navigate(Destination.Info.createRoute(it)) },
     ) {
         Column(
             modifier = Modifier
-                .padding(8.dp)
+                .padding(12.dp)
                 .fillMaxHeight(),
             verticalArrangement = Arrangement.Center
         ) {
@@ -209,7 +204,8 @@ fun PeriodProducts (navController: NavHostController, it: String) {
                     .padding(8.dp)
                     .align(Alignment.CenterHorizontally),
                 textAlign = TextAlign.Center,
-                text = it
+                text = it,
+                fontWeight = Bold
             )
         }
     }
@@ -223,10 +219,10 @@ fun PeriodProducts (navController: NavHostController, it: String) {
 fun TPPCard(uriHandler: UriHandler) {
     Card(
         modifier = Modifier
-            .padding(horizontal = 36.dp, vertical = 8.dp)
+            .padding(horizontal = 32.dp, vertical = 28.dp)
             .height(IntrinsicSize.Min),
         shape = RoundedCornerShape(12.dp),
-        elevation = 8.dp,
+        elevation = 10.dp,
         backgroundColor = Color.White,
     ) {
         Column(
@@ -251,7 +247,7 @@ fun TPPCard(uriHandler: UriHandler) {
                 .align(Alignment.CenterHorizontally),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Green,
+                    backgroundColor = Color(teal),
                     contentColor = Color.Black
                 ),
                 onClick = { uriHandler.openUri("https://www.theperiodpurse.com/") }) {
@@ -260,6 +256,109 @@ fun TPPCard(uriHandler: UriHandler) {
         }
     }
 }
+
+
+@Composable
+fun SocialMedia(uriHandler: UriHandler) {
+    Row(
+        modifier = Modifier
+            .wrapContentHeight()
+            .padding(6.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier
+                .clickable { uriHandler.openUri("https://www.google.ca/") }
+                .padding(horizontal = 8.dp)
+                .size(24.dp),
+            painter = painterResource(R.drawable.instagram),
+            contentDescription = "Instagram",
+            tint = Color(teal)
+        )
+
+        Icon(
+            modifier = Modifier
+                .clickable { uriHandler.openUri("https://www.google.ca/") }
+                .padding(horizontal = 8.dp)
+                .size(24.dp),
+            painter = painterResource(R.drawable.tiktok),
+            contentDescription = "Tik Tok",
+            tint = Color(teal)
+        )
+
+        Icon(
+            modifier = Modifier
+                .clickable { uriHandler.openUri("https://www.google.ca/") }
+                .padding(horizontal = 8.dp)
+                .size(24.dp),
+            painter = painterResource(R.drawable.youtube),
+            contentDescription = "YouTube",
+            tint = Color(teal)
+        )
+
+        Icon(
+            modifier = Modifier
+                .clickable { uriHandler.openUri("https://www.google.ca/") }
+                .padding(horizontal = 8.dp)
+                .size(24.dp),
+            painter = painterResource(R.drawable.twitter),
+            contentDescription = "Twitter",
+            tint = Color(teal)
+        )
+
+        Icon(
+            modifier = Modifier
+                .clickable { uriHandler.openUri("https://www.google.ca/") }
+                .padding(horizontal = 8.dp)
+                .size(24.dp),
+            painter = painterResource(R.drawable.facebook),
+            contentDescription = "Facebook",
+            tint = Color(teal)
+        )
+    }
+}
+
+
+@Composable
+fun TermsAndPrivacyFooter(navController: NavHostController) {
+    Row(
+        modifier = Modifier
+            .wrapContentHeight()
+            .padding(horizontal = 12.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+            ClickableText(
+                onClick = { navController.navigate(Destination.Terms.route) },
+                style = TextStyle(
+                    textDecoration = TextDecoration.Underline,
+                    color = Color(teal),
+                    textAlign = TextAlign.Center,
+                    fontSize = 14.sp
+                ),
+                text = AnnotatedString("Terms and Conditions")
+            )
+
+            Text(
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp,
+                text = " and "
+            )
+
+            ClickableText(
+                onClick = { navController.navigate(Destination.Privacy.route) },
+                style = TextStyle(
+                    textDecoration = TextDecoration.Underline,
+                    color = Color(teal),
+                    textAlign = TextAlign.Center,
+                    fontSize = 14.sp
+                    ),
+                text = AnnotatedString("Privacy Policy"),
+            )
+        }
+
+    }
 
 
 /**
