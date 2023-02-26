5e1060f8bebc43fd2be40075d7b82f4d6740f1c5
Manpreet Bhatti <manpreet@bhatti.net>
2023-02-26 01:43:19 -0500

    Add value for list of dates in AppUiState

--- app/src/main/java/com/example/theperiodpurse/AppUiState.kt
+++ app/src/main/java/com/example/theperiodpurse/AppUiState.kt
@@ -1,9 +1,11 @@
 package com.example.theperiodpurse
 
+import com.example.theperiodpurse.data.Date
 import com.example.theperiodpurse.data.Symptom
 
 // data class
 data class AppUiState (
     // Preffered Symptoms to be tracked
-    val trackedSymptoms: List<Symptom> = listOf()
+    val trackedSymptoms: List<Symptom> = listOf(),
+    val dates: List<Date> = emptyList(),
 )
