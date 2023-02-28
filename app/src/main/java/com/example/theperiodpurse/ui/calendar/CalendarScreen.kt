257e36c5426c4da0f5bd49c9ea342cd4b87e02d8
Madeline <madeline.ahn@mail.utoronto.ca>
2023-02-27 20:27:51 -0500

    Added bottom padding to CalendarScreen.kt

--- app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/calendar/CalendarScreen.kt
@@ -208,6 +208,7 @@ fun CalendarScreenLayout(calendarViewModel: CalendarViewModel, navController: Na
                     modifier = Modifier
                         .padding(horizontal = 12.dp)
                         .semantics { contentDescription = "Calendar" },
+                    contentPadding = PaddingValues(bottom = 48.dp),
                     state = state,
                     monthHeader = { month ->
                         MonthHeader(month)
@@ -229,6 +230,10 @@ fun CalendarScreenLayout(calendarViewModel: CalendarViewModel, navController: Na
                         }
                     }
                 )
+
+                Spacer(modifier = Modifier.size(36.dp))
+
+
             }
         }
     }
