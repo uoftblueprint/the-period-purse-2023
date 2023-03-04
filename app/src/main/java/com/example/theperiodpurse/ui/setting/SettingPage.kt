4bfc49a7f59d8ad95dc40edcd98f7e28c1d7bcf0
Kevin Le <kevinle6231@gmail.com>
2023-03-04 15:53:50 -0500

    fixed merge with develop

--- app/src/main/java/com/example/theperiodpurse/ui/setting/SettingPage.kt
+++ app/src/main/java/com/example/theperiodpurse/ui/setting/SettingPage.kt
@@ -1,23 +1,14 @@
 package com.example.theperiodpurse.ui.setting
 
 import android.os.Build
-import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.theperiodpurse.Application
 import com.example.theperiodpurse.MainActivity
 import com.example.theperiodpurse.ui.onboarding.GoogleSignOutButton
-import com.example.theperiodpurse.ui.onboarding.QuickStartButton
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
