0fac47b46857f543c479eae82a0811282a602002
leowrites <75044178+leowrites@users.noreply.github.com>
2023-04-02 19:31:05 -0400

    add sqlcipher

--- app/src/main/java/com/tpp/theperiodpurse/data/ApplicationRoomDatabase.kt
+++ app/src/main/java/com/tpp/theperiodpurse/data/ApplicationRoomDatabase.kt
@@ -5,6 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import net.sqlcipher.database.SQLiteDatabase.getBytes
+import net.sqlcipher.database.SupportFactory
 
 @Database(entities=[User::class, Date::class], version = 5, exportSchema = false)
 @TypeConverters(
@@ -21,11 +23,16 @@ abstract class ApplicationRoomDatabase: RoomDatabase() {
         private var INSTANCE: ApplicationRoomDatabase? = null
         fun getDatabase(context: Context): ApplicationRoomDatabase {
             return INSTANCE ?: synchronized(this) {
+                val passphrase: ByteArray = getBytes(
+                    "ltqZHr/glhTLg4iAD1xpOg5fh9mivIBWG2pZ6tw6qlw=".toCharArray()
+                )
+                val factory = SupportFactory(passphrase)
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     ApplicationRoomDatabase::class.java,
                     "user_database"
                 )
+                    .openHelperFactory(factory)
                     .fallbackToDestructiveMigration()
                     .build()
                 INSTANCE = instance
