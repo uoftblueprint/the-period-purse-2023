7667890662f0371df66e97a1548f4c002d468714
leowrites <75044178+leowrites@users.noreply.github.com>
2023-06-27 22:42:14 -0400

    Fix local time

--- app/src/main/java/com/tpp/theperiodpurse/ui/state/CalendarDayUIState.kt
+++ app/src/main/java/com/tpp/theperiodpurse/ui/state/CalendarDayUIState.kt
@@ -2,7 +2,7 @@ package com.tpp.theperiodpurse.ui.state
 
 
 import com.tpp.theperiodpurse.data.model.*
-import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 
@@ -17,14 +17,14 @@ data class CalendarDayUIState(
     private val formatter = DateTimeFormatter.ISO_LOCAL_TIME
 
     private val exerciseLength =
-        if (exerciseLengthString != "") LocalDateTime.parse(exerciseLengthString, formatter) else null
+        if (exerciseLengthString != "") LocalTime.parse(exerciseLengthString, formatter) else null
 
     val exerciseSeverity = if (exerciseLength != null) ExerciseSeverity.fromHoursAndMinutes(
         exerciseLength.hour,
         exerciseLength.minute
     ) else null
 
-    val sleep = if (sleepString != "")  LocalDateTime.parse(sleepString, formatter) else null
+    val sleep = if (sleepString != "")  LocalTime.parse(sleepString, formatter) else null
     val sleepScore =
         if (sleep != null) Sleep.fromHoursAndMinutes(sleep.hour, sleep.minute) else null
 }
