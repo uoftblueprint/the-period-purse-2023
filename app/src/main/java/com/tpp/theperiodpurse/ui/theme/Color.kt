13a0a2e8271d6d1124f932f0a7b4b185c8f42484
Manpreet-Bhatti <manpreet@bhatti.net>
2023-07-01 13:29:24 -0400

    Add colours for pink and gray

--- app/src/main/java/com/tpp/theperiodpurse/ui/theme/Color.kt
+++ app/src/main/java/com/tpp/theperiodpurse/ui/theme/Color.kt
@@ -8,6 +8,8 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Red = Color(0xFFBF3428)
 val Teal = Color(0xFF72C6B7)
+val Gray = Color(0xFF6D6E71)
+val Pink = Color(0xFFFFA3A4)
 
 val HeaderColor1 = Color(0xFFF1F1F1)
 val BottomBarColor1 = Color(0xFFFFFFFF)
