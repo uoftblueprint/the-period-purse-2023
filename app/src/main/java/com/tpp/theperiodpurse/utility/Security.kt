61f1cae228a4300093c199d6ac972603e8229846
leowrites <75044178+leowrites@users.noreply.github.com>
2023-07-05 12:19:40 -0400

    Add function for validation

---
+++ app/src/main/java/com/tpp/theperiodpurse/utility/Security.kt
@@ -0,0 +1,17 @@
+package com.tpp.theperiodpurse.utility
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.Scope
+import com.google.api.services.drive.DriveScopes
+
+fun validateUserAuthenticationAndAuthorization(account: GoogleSignInAccount?): Boolean {
+    if (account == null) return false
+    val requiredScopes = setOf(
+        Scope(DriveScopes.DRIVE_FILE),
+        Scope(DriveScopes.DRIVE_APPDATA)
+    )
+    if (account.grantedScopes.containsAll(requiredScopes)) {
+        return true
+    }
+    return false
+}
