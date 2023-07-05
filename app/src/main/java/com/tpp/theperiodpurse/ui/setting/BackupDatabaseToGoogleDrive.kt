d9e71460522d030d5bd31ed1081525418fd6e8a1
leowrites <75044178+leowrites@users.noreply.github.com>
2023-07-05 14:08:15 -0400

    clean up logic

--- app/src/main/java/com/tpp/theperiodpurse/ui/setting/BackupDatabaseToGoogleDrive.kt
+++ app/src/main/java/com/tpp/theperiodpurse/ui/setting/BackupDatabaseToGoogleDrive.kt
@@ -1,62 +1,54 @@
 package com.tpp.theperiodpurse.ui.setting
 
 
-import android.accounts.Account
 import android.content.Context
 import android.os.Build
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavHostController
-import com.tpp.theperiodpurse.ui.onboarding.LoadingScreen
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.tpp.theperiodpurse.ui.component.handleError
+import com.tpp.theperiodpurse.ui.component.LoadingScreen
 import com.tpp.theperiodpurse.ui.viewmodel.OnboardViewModel
+import com.tpp.theperiodpurse.utility.validateUserAuthenticationAndAuthorization
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
+// TODO Figure out why sometimes database is not backed up
 fun BackupDatabase(viewModel: OnboardViewModel,
                    navController: NavHostController,
-                   account: Account?,
                    signout: () -> Unit = {},
                    context: Context) {
 
-    val isBackedUp by viewModel.isBackedUp.observeAsState(initial = null)
-    val confirmLoad = remember { mutableStateOf(false) } // ensures that launched event only happens once
-    // (prevents multiple threads/runs from happening)
+    val hasBackedUpToGoogleDrive by viewModel.hasBackedUpToGoogleDrive.observeAsState()
+    val googleSignedInAccount = GoogleSignIn.getLastSignedInAccount(context)
+    val account = googleSignedInAccount?.account
+    val hasGoogleDrivePermission = validateUserAuthenticationAndAuthorization(googleSignedInAccount)
+    if (account == null) {
+        handleSecurityError(context, signout, "ERROR - Please sign in", navController)
+    }
+    if (!hasGoogleDrivePermission) {
+        handleSecurityError(context, signout, "ERROR - Please grant all the required permissions", navController)
+    }
 
-
-    LaunchedEffect(Unit){
-        if (account != null){
+    LaunchedEffect(key1 = hasBackedUpToGoogleDrive) {
+        if (account != null && !hasBackedUpToGoogleDrive!!) {
             viewModel.backupDatabase(account = account, context)
-        }
-
-    }
-    if (isBackedUp == null){
-        LoadingScreen()
-
-    }
-    else {
-        if (isBackedUp == true) {
-            LaunchedEffect(Unit){
-                viewModel.isBackedUp.postValue(null)
-                navController.navigate(SettingScreenNavigation.ConfirmBackup.name)
-            }
         } else {
-            if (!confirmLoad.value){
-                Toast.makeText(context, "ERROR - Please grant all the required permissions", Toast.LENGTH_SHORT).show()
-                signout()
-                confirmLoad.value = true
-                LaunchedEffect(Unit){
-                    viewModel.isBackedUp.postValue(null)
-                    navController.navigate(SettingScreenNavigation.Start.name)
-                }
-
-            }
+            viewModel.hasBackedUpToGoogleDrive.postValue(false)
+            navController.navigate(SettingScreenNavigation.ConfirmBackup.name)
         }
-
-
     }
+    LoadingScreen()
+}
 
+fun handleSecurityError(context: Context, signout: () -> Unit, msg: String, navController:
+NavHostController) {
+    handleError(context, msg) {
+        signout()
+        navController.navigate(SettingScreenNavigation.Start.name)
+    }
 }
